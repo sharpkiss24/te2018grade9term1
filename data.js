67a8c5c86c2004cc449b04c330df167614710437
shariana allen <shariana.allen@gmail.com>
2018-10-13 15:42:48 -0400

    updated data.js
    it was fun

--- data.js
+++ data.js
@@ -2,4 +2,22 @@
 let message = "i am a friendly person";
 console.log(message);
 
+let name="cherry is my name"
+console.log(name)
+
+let inlovewithBTS= true;
+
+console.log("in love with BTS");
+
+let idonothave6siblings=false;
+
+console.log("i do not have 6 siblings")
+
+let mycurrentage=14
+
+console.log("my current age is "  + mycurrentage)
+
+
+
+
 // Type your code below!
