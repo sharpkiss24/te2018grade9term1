14f1d541766b319c61bef0e6f24f8a6c330decdf
sharpkiss24 <shariana.allen@gmail.com>
2018-12-01 13:17:08 -0500

    not done

--- functions2.js
+++ functions2.js
@@ -1,7 +1,7 @@
 
 /******************************************************************************
                                    sumDouble(a, b)
-Instructions:
+Instructions: 
 Given two number values, return their sum. Unless the two values are the same,
 then return double their sum.
 
@@ -10,9 +10,21 @@ sumDouble(1, 2) → 3
 sumDouble(3, 2) → 5
 sumDouble(2, 2) → 8
 *******************************************************************************/
+function sumdouble(num1,num2){
+
+if(num1 == num2){
+
+  return (num1 + num2) * 2
 
 
+}else{
 
+    return num1 + num2
+  }
+
+}
+
+console.log(sumdouble(6,6));
 // Remove comment below to test this function.
 //testSumDouble();
 
@@ -27,13 +39,24 @@ makes10(9, 9) → false
 makes10(1, 9) → true
 *******************************************************************************/
 
+function makes10(a,b){
 
+  if(a + b == 10){
+       return true;
+  }
+  else if(a == 10 || b == 10){
 
+      return true;
+  }else{
+    return false;
+  }
+}
+    testMakes10();
 // Remove comment below to test this function.
 //testMakes10();
 
 /******************************************************************************
-                          parrotTrouble(talking, hour)
+                          parrotTrouble(talking,hour)
 Instructions:
 We have a loud talking parrot. The "talking" parameter is a boolean that
 represents if the parrot is talking. The "hour" parameter is the current hour
@@ -45,11 +68,25 @@ parrotTrouble(true, 6) → true
 parrotTrouble(true, 7) → false
 parrotTrouble(false, 6) → false
 *******************************************************************************/
+function parrotTrouble(talking,hour){
+  if(hour < 7 || hour > 20){
 
+      if(talking == true){
+        return true
+      }
+      else {
+        return false
+      }
+       
+  }
 
-
+  else{
+    return false 
+  }
+}
+   
 // Remove comment below to test this function.
-//testParrotTrouble();
+testParrotTrouble();
 
 /******************************************************************************
                              alarmClock(day, vacation)
@@ -65,6 +102,9 @@ alarmClock(1, false) → "7:00"
 alarmClock(6, true) → "off"
 alarmClock(0, false) → "10:00"
 *******************************************************************************/
+function alarmclock(day,vacation)
+    if(vacation == true )
+
 
 
 
@@ -97,11 +137,11 @@ caughtSpeeding(65, true) → 0
 *****************************************************************************/
 function testSumDouble() {
   console.log();
-  console.log("sumDouble(1, 2) → 3 "	+ sumDouble(1, 2));
+  console.log("sumDouble(1, 2) → 3 " + sumDouble(1, 2));
   console.log("sumDouble(3, 2) → 5 " + sumDouble(3, 2));
   console.log("sumDouble(2, 2) → 8 " + sumDouble(2, 2));
-  console.log("sumDouble(-1, 0) → -1 " + sumDouble(-1, 0));
-  console.log("sumDouble(3, 3) → 12 " + sumDouble(3, 3));
+  console.log("sumDouble(-1, 0) → -1" + sumDouble(-1, 0));
+  console.log("sumDouble(3, 3) → 12" + sumDouble(3, 3));
   console.log("sumDouble(0, 0) → 0 " + sumDouble(0, 0));
   console.log("sumDouble(0, 1) → 1 " + sumDouble(0, 1));
   console.log("sumDouble(3, 4) → 7 " + sumDouble(3, 4));
